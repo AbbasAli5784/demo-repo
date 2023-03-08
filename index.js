d7b6d2a9717af3cf2d3c26f8e4b5465b6936b9f7
Abbas Ali <abbasali5784@gmail.com>
2023-03-08 13:00:55 -0500

    Updated endpoints to interact with mongoDB database

--- index.js
+++ index.js
@@ -1,238 +1,199 @@
 const express = require("express");
 const app = express();
-//const morgan = require("morgan");
-//const fs = require("fs");
-//const path = require("path");
-//const { Stream } = require("stream");
-/*const accessLogStreamm = fs.createWriteStream(path.join(__dirname, "log.txt"), {
-  flags: "a",
-});*/
 const bodyParser = require("body-parser");
-const { uniqueId } = require("lodash");
+const { uniqueId, update } = require("lodash");
 const uuid = require("uuid");
-//const { allowedNodeEnvironmentFlags } = require("process");
-//const res = require("express/lib/response");
-//const { redirect } = require("express/lib/response");
+const mongoose = require("mongoose");
+const Models = require("./models.js");
+const Movies = Models.Movie;
+const Users = Models.User;
+const Genres = Models.Genre;
+const Directors = Models.Director;
+mongoose.connect("mongodb://localhost:27017/MyFlixDB", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-let users = [
-  { id: 1, name: "Katie", movie: [] },
-  { id: 2, name: "John", movie: ["SuperBad"] },
-];
-
-let favouritemovies = [
-  {
-    Title: "SuperBad",
-    DirectorInfo: {
-      Name: "Greg Mottola",
-      Bio: "asfasfhasjf",
-      BirthYear: 1972,
-      DeathYear: 2000,
-    },
-    Genre: "Comedy",
-    Description:
-      "Two high school seniors embark on a wild night of adventure in an attempt to buy alcohol for a party.",
-  },
-  {
-    Title: "Titanic",
-    DirectorInfo: {
-      Name: "James Cameron",
-      Bio: "asfasfhasjf",
-      BirthYear: 1972,
-      DeathYear: 2000,
-    },
-    Genre: "Drama/Romance",
-    Description:
-      "A love story between a wealthy woman and a poor artist on the ill-fated maiden voyage of the RMS Titanic.",
-  },
-  {
-    Title: "The Godfather",
-    DirectorInfo: {
-      Name: "Francis Ford Coppola",
-      Bio: "asfasfhasjf",
-      BirthYear: 1972,
-      DeathYear: 2000,
-    },
-    Genre: "Crime/Drama",
-    Description:
-      "The story of a powerful Italian-American mafia family and their patriarch, Vito Corleone.",
-  },
-  {
-    Title: "Forrest Gump",
-    DirectorInfo: {
-      Name: "Robert Zemeckis",
-      Bio: "asfasfhasjf",
-      BirthYear: 1972,
-      DeathYear: 2000,
-    },
-    Genre: "Drama/Comedy",
-    Description:
-      "A simple man with a low IQ experiences several key historical events and influences popular culture.",
-  },
-  {
-    Title: "The Dark Knight",
-    DirectorInfo: {
-      Name: "Christopher Nolan",
-      Bio: "asfasfhasjf",
-      BirthYear: 1972,
-      DeathYear: 2000,
-    },
-    Genre: "Action/Crime",
-    Description:
-      "Batman faces off against his greatest enemy, the Joker, in a battle to save Gotham City.",
-  },
-  {
-    Title: "The Lord of the Rings",
-    DirectorInfo: {
-      Name: "Peter Jackson",
-      Bio: "asfasfhasjf",
-      BirthYear: 1972,
-      DeathYear: 2000,
-    },
-    Genre: "Fantasy/Adventure",
-    Description:
-      "A young hobbit named Frodo Baggins must destroy an evil ring and save Middle-earth from darkness.",
-  },
-  {
-    Title: "Pulp Fiction",
-    DirectorInfo: {
-      Name: "Quentin Tarantino",
-      Bio: "asfasfhasjf",
-      BirthYear: 1972,
-      DeathYear: 2000,
-    },
-    Genre: "Crime/Drama",
-    Description:
-      "The intersecting stories of several characters in the criminal underworld of Los Angeles.",
-  },
-  {
-    Title: "Star Wars",
-    DirectorInfo: {
-      Name: "George Lucas",
-      Bio: "asfasfhasjf",
-      BirthYear: 1972,
-      DeathYear: 2000,
-    },
-    Genre: "Science Fiction",
-    Description:
-      "A group of rebels fight against the evil Empire in a galaxy far, far away.",
-  },
-  {
-    Title: "The Matrix",
-    DirectorInfo: {
-      Name: "The Wachowskis",
-      Bio: "asfasfhasjf",
-      BirthYear: 1972,
-      DeathYear: 2000,
-    },
-    Genre: "Science Fiction",
-    Description:
-      "A computer hacker learns the truth about reality and leads a rebellion against machines that have enslaved humanity.",
-  },
-  {
-    Title: "The Avengers",
-    DirectorInfo: {
-      Name: "Anthony Russo",
-      Bio: "asfasfhasjf",
-      BirthYear: 1972,
-      DeathYear: 2000,
-    },
-    Genre: "Action/Science Fiction",
-    Description:
-      "A team of superheroes, including Iron Man, Captain America, and Thor, team up to save the world from a powerful threat.",
-  },
-];
-
-//Exercise 2.4
-/*app.use(morgan("combined", { stream: accessLogStreamm }));
-
-app.get("/movies", (req, res) => {
-  res.json(favouritemovies);
-});
-
-app.get("/", (req, res) => {
-  res.send("Welcome to MyFlix!");
-});
-
-app.use(express.static("Public"));
-
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).send("Something Broke!");
-});*/
-
-//allow a new user to regiester
-
+//Allows a new user to regiester
 app.post("/users", (req, res) => {
-  const newUser = req.body;
-  if (newUser.name) {
-    newUser.id = uuid.v4();
-    users.push(newUser);
-    res.json(newUser);
-  }
+  Users.findOne({ Username: req.body.Username })
+    .then((user) => {
+      if (user) {
+        return res.status(400).send(req.body.Username + "already exists");
+      } else {
+        Users.create({
+          Username: req.body.Username,
+          Password: req.body.Password,
+          Email: req.body.Email,
+          Birthday: req.body.Birthday,
+        })
+          .then((user) => {
+            res.status(201).json(user);
+          })
+          .catch((error) => {
+            console.error(error);
+            res.status(500).send("Error: " + error);
+          });
+      }
+    })
+    .catch((error) => {
+      console.error(error);
+      res.status(500).send("Error: " + error);
+    });
 });
-//allow users to update username
+//Returns a list of all users
+app.get("/users", (req, res) => {
+  Users.find()
+    .then((users) => {
+      res.status(201).json(users);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error:" + err);
+    });
+  //Get a user by username
+  app.get("/users/:Username", (req, res) => {
+    Users.findOne({ Username: req.params.Username })
+      .then((user) => {
+        res.json(user);
+      })
+      .catch((err) => {
+        console.error(err);
+        res.status(500).send("Error:" + err);
+      });
+  });
+});
+//update a user's info, by username
+
 app.put("/users/:id", (req, res) => {
-  const { id } = req.params;
-  const updateUser = req.body;
-  let user = users.find((user) => user.id == id);
-  res.status(200).json(user);
+  console.log("Updating user:", req.params.id);
+  console.log("New user data:", req.body);
+
+  Users.findByIdAndUpdate(
+    req.params.id,
+    {
+      $set: {
+        Username: req.body.Username,
+        Password: req.body.Password,
+        Email: req.body.Email,
+        Birthday: req.body.Birthday,
+      },
+    },
+    { new: true }
+  )
+    .then((updatedUser) => {
+      console.log(updatedUser);
+      res.json(updatedUser);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error:" + err);
+    });
 });
 
-//allow users to add a list of movies to there favourites
+//Add a movie to a user's list of favorites
+app.post("/users/:Username/movies/:MovieID", (req, res) => {
+  Users.findOneAndUpdate(
+    { Username: req.params.Username },
+    { $push: { FavoriteMovies: req.params.MovieID } },
+    { new: true }
+  )
+    .then((updateUser) => {
+      res.json(updateUser);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error: " + err);
+    });
+});
 
-app.post("/movies/:id/:movieTitle", (req, res) => {
-  const { id, movieTitle } = req.params;
-  const user = users.find((user) => user.id == id);
-  if (user) {
-    user.movie.push(movieTitle);
-    res.send("movie has been added to user!");
-  }
+//Allow a user to deregiester 
+app.delete("/users/:Username", (req, res) => {
+  Users.findOneAndRemove({ Username: req.params.Username })
+    .then((user) => {
+      if (!user) {
+        res.status(400).send(req.params.Username + "was not found");
+      } else {
+        res.status(200).send(req.params.Username + " was deleted.");
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error: " + err);
+    });
 });
 
 //Allow users to remove a movie from there list
 
-app.delete("/movies/:id/:movieTitle", (req, res) => {
-  res.send("Movie has been removed from list");
-});
-
-//Allow a user to deregiester
-app.delete("/users/:id", (req, res) => {
-  res.send("User has been deleted");
+app.put("/users/:Username/movies/:MovieID", (req, res) => {
+  Users.updateOne(
+    { Username: req.params.Username },
+    { $pull: { FavoriteMovies: req.params.MovieID } },
+    { new: true }
+  )
+    .then((user) => {
+      res.json(user);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error: " + err);
+    });
 });
 
 // Returns list of ALL movies to user
 app.get("/movies", (req, res) => {
-  res.status(200).json(favouritemovies);
+  Movies.find().then((movies) => {
+    res.status(201).json(movies);
+  });
 });
 
 //Returns data about a single movie by Title to the user
 app.get("/movies/:title", (req, res) => {
-  const { title } = req.params;
-  console.log(title);
-  const movie = favouritemovies.find((movie) => movie.Title === title);
-  res.json(movie);
+  Movies.findOne({ Name: req.params.title })
+    .then((movie) => {
+      res.json(movie);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error:" + err);
+    });
 });
 
 //Get movies by Genre name
 app.get("/movies/genre/:genreName", (req, res) => {
-  const { genreName } = req.params;
-  const genre = favouritemovies.filter((movie) => movie.Genre === genreName);
-  res.status(200).json(genre);
+  Genres.findOne({ Name: req.params.genreName })
+    .then((genres) => {
+      res.json(genres);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error:" + err);
+    });
 });
-
-
 
 app.get("/movies/directors/:directorName", (req, res) => {
-  const { directorName } = req.params;
-  const director = favouritemovies.find(
-    (movie) => movie.DirectorInfo.Name === directorName
-  );
-  res.status(200).json(director.DirectorInfo);
-  console.log(director);
+  Directors.findOne({ Name: req.params.directorName })
+    .then((directors) => {
+      res.json(directors);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error:" + err);
+    });
 });
 
-
+app.get("/genres", (req, res) => {
+  Genres.find()
+    .then((genres) => {
+      res.status(201).json(genres);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error:" + err);
+    });
+});
 
 app.listen(8080, () => console.log("listening on 8080"));
