abcb547532d5f1c864a248fe0adea0d58ccd4ebd
Abbas Ali <abbasali5784@gmail.com>
2023-03-10 11:18:36 -0500

    Having an issue connecting to mongoserver

--- index.js
+++ index.js
@@ -9,7 +9,11 @@ const Movies = Models.Movie;
 const Users = Models.User;
 const Genres = Models.Genre;
 const Directors = Models.Director;
-mongoose.connect("mongodb://localhost:27017/MyFlixDB", {
+// mongoose.connect("mongodb://localhost:27017/MyFlixDB", {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// });
+mongoose.connect(process.env.CONNECTION_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
