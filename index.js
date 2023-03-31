b9db96f891a04875e16c27f0459d53f41e01487c
Abbas Ali <abbasali5784@gmail.com>
2023-03-30 21:04:57 -0400

    added auth again

--- index.js
+++ index.js
@@ -184,11 +184,15 @@ app.put("/users/:Username/movies/:MovieID", (req, res) => {
 });
 
 // Returns list of ALL movies to user
-app.get("/movies", (req, res) => {
-  Movies.find().then((movies) => {
-    res.status(201).json(movies);
-  });
-});
+app.get(
+  "/movies",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    Movies.find().then((movies) => {
+      res.status(201).json(movies);
+    });
+  }
+);
 
 //Returns data about a single movie by Title to the user
 app.get(
