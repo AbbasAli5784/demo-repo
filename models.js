f20cc036937cd77ecf6ef74357014091aff25b6d
Abbas Ali <abbasali5784@gmail.com>
2023-09-20 18:21:07 -0400

    Reverted Schema

--- models.js
+++ models.js
@@ -18,7 +18,7 @@ let userSchema = mongoose.Schema({
   Password: { type: String, required: true },
   Email: { type: String, required: true },
   Birthday: Date,
-  FavoriteMovies: [movieSchema],
+  FavoriteMovies: [{ type: mongoose.Schema.Types.ObjectId, ref: "Movie" }],
 });
 userSchema.statics.hashPassword = (password) => {
   return bcrypt.hashSync(password, 10);
